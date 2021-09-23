785f6f2cb7262e9b1871e8bd933c16e8478b9607
anuakshat <53713926+anuakshat@users.noreply.github.com>
2021-09-23 18:09:28 +0530

    updated conditions for empty and negative inputs

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ function anglesCalculation(angle1,angle2,angle3){
 
 function isThisTriangle()
 {
-    if(!Number(angleInput[0].value)|| !Number(angleInput[1].value) || !Number(angleInput[2].value)){
-    const anglesSum=anglesCalculation(Number(angleInput[0].value),Number(angleInput[1].value),Number(angleInput[2].value));
+    if(!Number(angleInput[0].value)|| !Number(angleInput[1].value) || !Number(angleInput[2].value))
+    {
+        outputEl.innerText="Please enter all the fields";
+    }
+    else
+    { 
+        if(Number(angleInput[0].value)<1 || Number(angleInput[1].value)<1 || Number(angleInput[2].value)<1)
+        {outputEl.innerText="Please enter valid values";}
+    else{
+        const anglesSum=anglesCalculation(Number(angleInput[0].value),Number(angleInput[1].value),Number(angleInput[2].value));
    
-    if(anglesSum===180){outputEl.innerText="this is a triangle"}
-    else{outputEl.innerText="this is not a triangle"}
-    
+        if(anglesSum===180){outputEl.innerText="this is a triangle"}
+        else{outputEl.innerText="this is not a triangle"}
+    }
 }
-else{
-    outputEl.innerText=""
-}
-    
 }
 
 checkButton.addEventListener("click",isThisTriangle);
