a501842cb4e6b5f41bc250ecdc441e551d1cb8e4
anuakshat <53713926+anuakshat@users.noreply.github.com>
2021-09-23 17:43:37 +0530

    updated condition

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ function anglesCalculation(angle1,angle2,angle3){
 
 function isThisTriangle()
 {
+    if(!Number(angleInput[0].value)|| !Number(angleInput[1].value) || !Number(angleInput[2].value)){
     const anglesSum=anglesCalculation(Number(angleInput[0].value),Number(angleInput[1].value),Number(angleInput[2].value));
-   //console.log(anglesSum);
-if(anglesSum===180){outputEl.innerText="this is a triangle"}else{outputEl.innerText="this is not a triangle"}
-
+   
+    if(anglesSum===180){outputEl.innerText="this is a triangle"}
+    else{outputEl.innerText="this is not a triangle"}
+    
+}
+else{
+    outputEl.innerText=""
+}
+    
 }
 
 checkButton.addEventListener("click",isThisTriangle);
