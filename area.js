8b413628f1f9f8df47f6315ce705b43a9fc0c00a
anuakshat <53713926+anuakshat@users.noreply.github.com>
2021-09-23 18:21:51 +0530

    Conditions for negative and blank inputs applied

--- area.js
+++ area.js
@@ -9,9 +9,19 @@ return area
 }
 
 function areaCalculator(){
-    
+    if(!Number(lengthInput[0].value) || !Number(lengthInput[1].value)){
+      outputEl.innerText="Please fill all the values"
+    }
+    else{
+      if(Number(lengthInput[0].value)<1 || Number(lengthInput[1].value)<1)
+{
+  outputEl.innerText="Please enter valid values"
+}else{
   let triangelArea=areaOftriangle(Number(lengthInput[0].value),Number(lengthInput[1].value));
-outputEl.innerText="area of triangle is "+triangelArea;
+  outputEl.innerText="area of triangle is "+triangelArea;
+}
+    }
+  
 }
 
 checkButton.addEventListener("click",areaCalculator);
--- hypotaneous.js
+++ hypotaneous.js
@@ -9,12 +9,22 @@ return sumOfSquares
 }
 
 function hypoteneousCalculator(){
-    
-   let sum= squaresAndSum(Number(lengthInput[0].value),Number(lengthInput[1].value));
+    if(!Number(lengthInput[0].value)||!Number(lengthInput[1].value))
+    {
+        outputEl.innerText="Please Fill all the fields"
+    }
+    else{
+        if(Number(lengthInput[0].value)<1 ||Number(lengthInput[1].value)<1){
+            outputEl.innerText="please fill valid values";
+        }
+        else{
+            let sum= squaresAndSum(Number(lengthInput[0].value),Number(lengthInput[1].value));
     
     const squareRootSum=Math.sqrt(sum);
  const squareRootOfSum=squareRootSum.toFixed(2);
 outputEl.innerText="length of hypoteneus is "+squareRootOfSum;
-}
+        }
+    }
+   }
 
 checkButton.addEventListener("click",hypoteneousCalculator);
